53023258624f95b0fa2177b0a97442f49041ddbf
Bolaji Fatade <bjfatade@gmail.com>
2016-06-07 13:40:07 -0400

    Fix compiler errors

--- interceptor.c
+++ interceptor.c
@@ -348,8 +348,8 @@ int error_check(int cmd, int syscall, int pid){
 
 			//spin_unlock(&pidlist_lock);
 			//spin_unlock(&calltable_lock);
-		if (pid == 0 and current_uid() == 0){
-
+		if (pid == 0 && current_uid() == 0){
+			//do nothing i.e don't return EINVAL
 		}else{
 
 			return -EINVAL;
